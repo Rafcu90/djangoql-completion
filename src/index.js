0581ad89c5990a36b65cf291e3756fb4e4b904d1
Rafcu90 <160595819+Rafcu90@users.noreply.github.com>
2025-11-24 14:35:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -312,14 +312,29 @@ DjangoQL.prototype = {
     // https://cypressnorth.com/programming/internet-explorer-aborting-ajax-requests-fixed/
     /* eslint-enable max-len */
     this.request.onprogress = function () {};
-    window.setTimeout(this.request.send.bind(this.request));
+    let prep;
+    if (this.options.prepareXhr) {
+      // Can be used to add extra headers
+      prep = this.options.prepareXhr(this.request)
+    }
+    else {
+      prep = Promise.resolve(this.request)
+    }
+    prep.then(() => {
+      window.setTimeout(this.request.send.bind(this.request));
+    }).catch(e => {
+      onLoadError()
+      this.logError("options.prepareXhr returned error", e)
+    })
   },
 
   loadIntrospections(introspections) {
     const initIntrospections = function (data) {
       this.currentModel = data.current_model;
       this.models = data.models;
-      this.suggestionsAPIUrl = data.suggestions_api_url;
+      if (!this.suggestionsAPIUrl) {
+        this.suggestionsAPIUrl = data.suggestions_api_url;
+      }
     }.bind(this);
 
     if (typeof introspections === 'string') {
